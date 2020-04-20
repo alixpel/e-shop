1804a5cbd2e13703bbd229bf65daea9fc4de42ce
alixpel <alixpelletierpro@gmail.com>
2020-04-20 10:53:28 +0200

    fix bonus dynamics and layout

--- assets/js/e-shop-script.js
+++ assets/js/e-shop-script.js
@@ -1,19 +1,50 @@
+
+// Menu +/-
+
+// affiliate
+// Ouverture de la page
+    document.querySelector('.bonus-minus-group-affiliate').style.display = "none";
+
 // Ouverture du menu bonus
-
-    document.querySelector('.bonus-minus').style.display = "none";
-    document.querySelector('.bonus-text').style.display = 'none';
-
-
-document.querySelector('.bonus-plus').addEventListener('mouseover', function(){
-    document.querySelector('.bonus-plus').style.display = "none";
-    document.querySelector('.bonus-minus').style.display = "block";
-    document.querySelector('bonus-text').style.display = 'block';
+document.querySelector('.bonus-plus-affiliate').addEventListener('mouseover', function(){
+    document.querySelector('.bonus-plus-affiliate').style.display = "none";
+    document.querySelector('.bonus-minus-group-affiliate').style.display = "block";
 });
 
 // Fermeture du menu bonus
+document.querySelector('.bonus-minus-group-affiliate').addEventListener('mouseout', function(){
+    document.querySelector('.bonus-plus-affiliate').style.display = "flex";
+    document.querySelector('.bonus-minus-group-affiliate').style.display = "none";
+});
 
-document.querySelector('.bonus-minus').addEventListener('mouseout', function(){
-    document.querySelector('.bonus-plus').style.display = "block";
-    document.querySelector('.bonus-minus').style.display = "none";
-    document.querySelector('bonus-text').style.display = 'none';
+// voucher
+// Ouverture de la page
+    document.querySelector('.bonus-minus-group-voucher').style.display = "none";
+
+// Ouverture du menu bonus
+document.querySelector('.bonus-plus-voucher').addEventListener('mouseover', function(){
+    document.querySelector('.bonus-plus-voucher').style.display = "none";
+    document.querySelector('.bonus-minus-group-voucher').style.display = "block";
+});
+
+// Fermeture du menu bonus
+document.querySelector('.bonus-minus-group-voucher').addEventListener('mouseout', function(){
+    document.querySelector('.bonus-plus-voucher').style.display = "flex";
+    document.querySelector('.bonus-minus-group-voucher').style.display = "none";
+});
+
+// cards
+// Ouverture de la page
+    document.querySelector('.bonus-minus-group-cards').style.display = "none";
+
+// Ouverture du menu bonus
+document.querySelector('.bonus-plus-cards').addEventListener('mouseover', function(){
+    document.querySelector('.bonus-plus-cards').style.display = "none";
+    document.querySelector('.bonus-minus-group-cards').style.display = "block";
+});
+
+// Fermeture du menu bonus
+document.querySelector('.bonus-minus-group-cards').addEventListener('mouseout', function(){
+    document.querySelector('.bonus-plus-cards').style.display = "flex";
+    document.querySelector('.bonus-minus-group-cards').style.display = "none";
 });
